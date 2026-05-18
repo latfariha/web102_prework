f4c3c59302db1a3d69e23fe651ef52fad5b67f8f
Fariha Lateef <farihalateef@gmail.com>
2026-05-17 22:54:57 -0700

    challege 7 done

--- index.js
+++ index.js
@@ -166,7 +166,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...rest] = sortedGames; 
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstgame = document.createElement("p");
+firstgame.innerHTML = firstGame.name;
+firstGameContainer.appendChild(firstgame);
 
 // do the same for the runner up item
+const secondgame = document.createElement("p");
+secondgame.innerHTML = secondGame.name;
+secondGameContainer.appendChild(secondgame);
